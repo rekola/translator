7d38d51ba2f918352baaf18d3a9ff7256ec6b4cf
Mikael Rekola <mikael.rekola@gmail.com>
2022-10-03 16:35:58 +0300

    enable Post request, increase payload limit

--- src/main.cpp
+++ src/main.cpp
@@ -19,35 +19,40 @@ int main(int argc, char ** argv) {
 #else
   httplib::Server svr;
 #endif
-    
-  svr.Get("/translate", [](const httplib::Request & req, httplib::Response & res) {
-    string q, source, target, format;
-    if (req.has_param("source")) source = req.get_param_value("source");
-    if (req.has_param("target")) target = req.get_param_value("target");
-    if (req.has_param("format")) format = req.get_param_value("format");
 
-    auto num_input = req.get_param_value_count("q");
-    std::vector<std::string> input;
-    input.reserve(num_input);
-    for (size_t i = 0; i < num_input; i++) input.push_back(req.get_param_value("q", i));
-    
-    auto & translator = TranslationContext::getInstance();
-    json translations;
+  svr.set_payload_max_length(1024 * 1024 * 512); // 512MB
 
-    for (auto & s : input) {
-      auto output = translator.translate(source, target, s);
-      cerr << "translated (" << source << " => " << target << "): \"" << s << "\" => \"" << output << "\"\n";
-
-      json translation;
-      translation["translatedText"] = output;
-      translations.push_back(translation);
-    }
-    
-    json payload;
-    payload["data"]["translations"] = translations;
-    
-    res.set_content(payload.dump(), "application/json");
-  });
+  auto handler = [](const httplib::Request & req, httplib::Response & res) {
+		   string q, source, target, format;
+		   if (req.has_param("source")) source = req.get_param_value("source");
+		   if (req.has_param("target")) target = req.get_param_value("target");
+		   if (req.has_param("format")) format = req.get_param_value("format");
+		   
+		   auto num_input = req.get_param_value_count("q");
+		   std::vector<std::string> input;
+		   input.reserve(num_input);
+		   for (size_t i = 0; i < num_input; i++) input.push_back(req.get_param_value("q", i));
+		   
+		   auto & translator = TranslationContext::getInstance();
+		   json translations;
+		   
+		   for (auto & s : input) {
+		     auto output = translator.translate(source, target, s);
+		     cerr << "translated (" << source << " => " << target << "): \"" << s << "\" => \"" << output << "\"\n";
+		     
+		     json translation;
+		     translation["translatedText"] = output;
+		     translations.push_back(translation);
+		   }
+		   
+		   json payload;
+		   payload["data"]["translations"] = translations;
+		   
+		   res.set_content(payload.dump(), "application/json");
+		 };
+  
+  svr.Get("/translate", handler);
+  svr.Post("/translate", handler);
 
   svr.Get("/index.html", [](const httplib::Request & req, httplib::Response & res) {
     res.set_content("Nothing here.", "text/plain");
