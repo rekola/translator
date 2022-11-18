cbecdccb7712771ff598a1e208fe0a0f98671ba0
Mikael Rekola <mikael.rekola@gmail.com>
2022-11-18 16:38:12 +0200

    update HTTP handlers

--- src/main.cpp
+++ src/main.cpp
@@ -25,59 +25,72 @@ int main(int argc, char ** argv) {
 
   svr.set_payload_max_length(1024 * 1024 * 512); // 512MB
 
-  auto handler = [](const httplib::Request & req, httplib::Response & res) {
-		   string q, source, target, format;
-		   if (req.has_param("source")) source = req.get_param_value("source");
-		   if (req.has_param("target")) target = req.get_param_value("target");
-		   if (req.has_param("format")) format = req.get_param_value("format");
+  auto translate_handler = [](const httplib::Request & req, httplib::Response & res) {
+    string q, source, target, format;
+    if (req.has_param("source")) source = req.get_param_value("source");
+    if (req.has_param("target")) target = req.get_param_value("target");
+    if (req.has_param("format")) format = req.get_param_value("format");
 
-		   auto & detector = LanguageDetector::getInstance();
+    auto & detector = LanguageDetector::getInstance();
 
-		   auto num_input = req.get_param_value_count("q");
-		   std::unordered_map<std::string, std::vector<std::pair<std::string, size_t>>> input;
-		   for (size_t i = 0; i < num_input; i++) {
-		     auto text = req.get_param_value("q", i);
-		     auto lang = !source.empty() ? source : detector.detectLanguage(text);
-		     input[lang].push_back(pair(text, i));
-		   }
+    auto num_input = req.get_param_value_count("q");
+    std::unordered_map<std::string, std::vector<std::pair<std::string, size_t>>> input;
+    for (size_t i = 0; i < num_input; i++) {
+      auto text = req.get_param_value("q", i);
+      auto lang = !source.empty() ? source : detector.detectLanguage(text);
+      input[lang].push_back(pair(text, i));
+    }
 
-		   std::vector<pair<string, string>> output;
-		   output.resize(num_input);
+    std::cerr << "input size: " << num_input << "\n";
+
+    std::vector<pair<string, string>> output;
+    output.resize(num_input);
 				 
-		   auto & translator = TranslationContext::getInstance();
-		   for (auto & [ lang, data ] : input) {
-		     std::vector<std::string> data2;
-		     for (auto & [s, pos] : data) data2.push_back(s);
-		     auto data3 = translator.translate(lang, target, data2);
-		     for (size_t i = 0; i < data.size(); i++) {
-		       auto & [s, pos] = data[i];
-		       std::string s2;
-		       if (i < data3.size()) {
-			 s2 = std::move(data3[i]);
-		       }
-		       cerr << "translated (" << lang << " => " << target << "): \"" << s << "\" => \"" << s2 << "\"\n";
-		       output[pos] = pair(s2, lang);
-		     }
-		   }
+    auto & translator = TranslationContext::getInstance();
+    for (auto & [ lang, data ] : input) {
+      std::vector<std::string> data2;
+      for (auto & [s, pos] : data) data2.push_back(s);
+      auto data3 = translator.translate(lang, target, data2);
+      for (size_t i = 0; i < data.size(); i++) {
+	auto & [s, pos] = data[i];
+	std::string s2;
+	if (i < data3.size()) {
+	  s2 = std::move(data3[i]);
+	}
+	cerr << "translated (" << lang << " => " << target << "): \"" << s << "\" => \"" << s2 << "\"\n";
+	output[pos] = pair(s2, lang);
+      }
+    }
 		   
-		   json translations;
+    json translations;
 		   
-		   for (auto & [s, lang] : output) {
-		     json translation;
-		     if (!s.empty()) translation["translatedText"] = s;
-		     if (source.empty()) translation["detectedSourceLanguage"] = lang;
+    for (auto & [s, lang] : output) {
+      json translation = json::object();
+      // if (!orig.empty()) translation["originalText"] = orig;
+      if (!s.empty()) translation["translatedText"] = s;
+      if (source.empty() && !lang.empty()) translation["detectedSourceLanguage"] = lang;
 		     
-		     translations.push_back(translation);
-		   }
+      translations.push_back(translation);
+    }
 		   
-		   json payload;
-		   payload["data"]["translations"] = translations;
+    json payload;
+    payload["data"]["translations"] = translations;
 		   
-		   res.set_content(payload.dump(), "application/json");
-		 };
+    res.set_content(payload.dump(), "application/json");
+  };
   
-  svr.Get("/translate", handler);
-  svr.Post("/translate", handler);
+  auto search_handler = [](const httplib::Request & req, httplib::Response & res) {
+    res.set_content("Not implemented", "text/plain");
+  };
+
+  auto status_handler = [](const httplib::Request & req, httplib::Response & res) {
+    res.set_content("Not implemented", "text/plain");    
+  };
+
+  svr.Get("/translate", translate_handler);
+  svr.Post("/translate", translate_handler);
+  svr.Get("/search", search_handler);
+  svr.Get("/status", status_handler);
 
   svr.Get("/index.html", [](const httplib::Request & req, httplib::Response & res) {
     res.set_content("Nothing here.", "text/plain");
