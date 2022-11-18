fdeef552836c12a2f5da697e9b084d59b71ceb4e
Mikael Rekola <mikael.rekola@gmail.com>
2022-11-18 16:42:37 +0200

    remove mutex and atomic variables, add fi-ru pair

--- src/TranslationContext.cpp
+++ src/TranslationContext.cpp
@@ -2,11 +2,11 @@
 
 #include "MarianTranslator.h"
 
-std::mutex TranslationContext::instance_mutex;
-std::atomic<TranslationContext *> TranslationContext::instance{nullptr};
-
 TranslationContext::TranslationContext() {
   addTranslator(std::make_unique<MarianTranslator>("en", "fi"));
+
+  addTranslator(std::make_unique<MarianTranslator>("fi", "ru"));
+  
   addTranslator(std::make_unique<MarianTranslator>("fi", "en"));
   addTranslator(std::make_unique<MarianTranslator>("sv", "en"));
   addTranslator(std::make_unique<MarianTranslator>("et", "en"));
